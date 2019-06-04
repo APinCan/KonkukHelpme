884cb7f9c384b8ed8dddba6f4e3303c34b5f5f34
BeeboyZ <youhw1995@naver.com>
2019-06-04 15:41:01 +0900

    수정중

--- Ilgamlake.cpp
+++ Ilgamlake.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "conio.h"
 #include "windows.h"
+#include "time.h"
 using namespace std;
 
 const int screenWidth = 60, screenHeight = 35;
@@ -109,11 +110,11 @@ public:
 	inline void CalcBodyPos() {
 		if (pivotPos.x > screenWidth) {
 			pivotPos.x = -1;
-			speed = rand() % 3 + 2;
+			speed = rand() % 2 + 1;
 		}
 		if (pivotPos.x < -1) {
 			pivotPos.x = screenWidth - 1;
-			speed = rand() % 3 + 2;
+			speed = rand() % 2 + 1;
 		}
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
@@ -185,7 +186,7 @@ public:
 		gameOver = false;
 		player = new Player(width / 2 - 1, height - 3);
 		for (int i = 0; i < 10; i++) {
-			int randomSpeed = rand() % 3 + 2;
+			int randomSpeed = rand() % 2 + 1;
 			{
 				if (i % 2 == 0) {
 					obs[i] = new Obstacle(2, i * 3 + 2, 2, randomSpeed, '>');
@@ -200,6 +201,7 @@ public:
 	~GameManager() {
 		delete player, obs;
 	}
+	/*
 	void Reset() {
 		score = 0;
 		gameOver = false;
@@ -208,6 +210,7 @@ public:
 			obs[t]->Reset();
 		}
 	}
+	*/
 	void CheckInput() {
 		if (_kbhit()) {
 			char current = _getch();
@@ -318,8 +321,8 @@ public:
 		for (int i = 0; i < 5; i++) { cout << "                                                                 " << endl; }
 		cout << " " << "###################################################################################" << endl;
 		cout << " " << "                                  °ÔÀÓ ¿À¹ö ¤Ð_¤Ð                                   " << endl;
-		cout << " " << "                            È¹µæÇÑ ÃÑ Á¡¼ö´Â " << score << "Á¡ ÀÔ´Ï´Ù.                  " << endl;
-		cout << " " << "                                     " << grade << " ÇÐÁ¡!!                                       " << endl;
+		cout << " " << "                            È¹µæÇÑ ÃÑ Á¡¼ö´Â " << score << "Á¡ ÀÔ´Ï´Ù.              " << endl;
+		cout << " " << "                                     " << grade << " ÇÐÁ¡!!                         " << endl;
 		cout << " " << "###################################################################################" << endl;
 
 		for (int i = 0; i < 3; i++) { cout << "                                                                 " << endl; }
